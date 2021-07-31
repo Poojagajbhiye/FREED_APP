d749c4f4940869f32227e2fc33c82a9d55bc2f2a
Roshan nahak <roshannahak1122@gmail.com>
2021-07-31 20:53:42 +0530

    added theme primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Colors;
 import 'package:freed/screen/SplashScreen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:freed/value/Colors.dart';
 
 void main() {
   runApp(
     ScreenUtilInit(
       designSize: Size(360, 780),
       builder: () => MaterialApp(
+        theme: ThemeData(colorScheme: ColorScheme.light(primary: Colors.black)),
         debugShowCheckedModeBanner: false,
         title: "Freed",
         home: SplashScreen(),
