97256f7b30aab960c856d8bc5d6027b5ed22b542
Roshan nahak <roshannahak1122@gmail.com>
2021-08-13 00:54:09 +0530

    init web socket and listner

--- lib/screen/FreedNotification.dart
+++ lib/screen/FreedNotification.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 class FreedNotification extends StatefulWidget {
   @override
@@ -8,10 +11,35 @@ class FreedNotification extends StatefulWidget {
 }
 
 class _FreedNotification extends State<FreedNotification> {
+  final channel = IOWebSocketChannel.connect(
+      Uri.parse("wss://dco-leave-app-api.herokuapp.com"));
+
+  @override
+  void initState() {
+    channel.stream.listen((message) {
+      channel.sink.add("6109de65a8529e00042a7fd8");
+      //channel.sink.close(status.goingAway);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Notification page is under development..")),
-    );
+        appBar: AppBar(
+          title: Text("web socket"),
+        ),
+        body: Container(
+          width: 1.sw,
+          height: 1.sh,
+          child: Center(
+            child: StreamBuilder(
+                stream: channel.stream,
+                builder: (context, snapshot) {
+                  return Text(
+                      "connection response ${snapshot.hasData ? snapshot.data : 'null'}");
+                }),
+          ),
+        ));
   }
 }
