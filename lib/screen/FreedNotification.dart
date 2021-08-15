1c54a84502b2231c567b6e09fcddc6e96fd92fe4
Roshan nahak <roshannahak1122@gmail.com>
2021-08-16 02:20:26 +0530

    [done] socket connection

--- lib/screen/FreedNotification.dart
+++ lib/screen/FreedNotification.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/status.dart' as status;
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:socket_io_client/socket_io_client.dart';
 
 class FreedNotification extends StatefulWidget {
   @override
@@ -11,15 +13,14 @@ class FreedNotification extends StatefulWidget {
 }
 
 class _FreedNotification extends State<FreedNotification> {
-  final channel = IOWebSocketChannel.connect(
-      Uri.parse("wss://dco-leave-app-api.herokuapp.com"));
+  // StreamSocket streamSocket = StreamSocket();
+  Socket? socket;
+  TextEditingController textEditingController = TextEditingController();
+  StreamController streamController = StreamController<String>();
 
   @override
   void initState() {
-    channel.stream.listen((message) {
-      channel.sink.add("6109de65a8529e00042a7fd8");
-      //channel.sink.close(status.goingAway);
-    });
+    connectToServer();
     super.initState();
   }
 
@@ -30,16 +31,96 @@ class _FreedNotification extends State<FreedNotification> {
           title: Text("web socket"),
         ),
         body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 50),
           width: 1.sw,
           height: 1.sh,
           child: Center(
-            child: StreamBuilder(
-                stream: channel.stream,
-                builder: (context, snapshot) {
-                  return Text(
-                      "connection response ${snapshot.hasData ? snapshot.data : 'null'}");
-                }),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: textEditingController,
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(hintText: "enter message here"),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      String inputmsg = textEditingController.text;
+                      sendMsgtoServer(inputmsg);
+                    },
+                    child: Text("Send")),
+                SizedBox(height: 20),
+                StreamBuilder(
+                    stream: streamController.stream,
+                    builder: (context, snapshot) {
+                      return Text("response: ${snapshot.data}");
+                    })
+              ],
+            ),
           ),
         ));
   }
+
+  sendMsgtoServer(String msg) {
+    socket?.emit("msg from admin", msg);
+  }
+
+  connectToServer() {
+    try {
+      socket =
+          IO.io('https://dco-leave-app-api.herokuapp.com/', <String, dynamic>{
+        'transports': ['websocket'],
+        'autoConnect': false,
+      });
+
+      socket?.connect();
+
+      socket?.onConnect((data) => print("connected"));
+      //socket?.emit("msg from admin", "hey im roshan");
+      print(socket?.connected);
+      socket?.on("msg to student", (data) => streamController.sink.add(data));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //STEP2: Add this function in main function in main.dart file and add incoming data to the stream
+  // void connectAndListen() {
+  //   try {
+  //     socket = io('https://dco-leave-app-api.herokuapp.com/',
+  //         OptionBuilder().setTransports(['websocket']).build());
+
+  //     socket?.onConnect((_) {
+  //       print('connect: ${socket?.id}');
+  //       socket?.emit('msg from admin', 'test message');
+  //     });
+
+  //     //When an event recieved from server, data is added to the stream
+  //     socket?.on('message', (data) => streamSocket.addResponse);
+  //     //socket?.on('typing', fromAdmin());
+  //     socket?.onDisconnect((_) => print('disconnect'));
+  //   } catch (e) {
+  //     print(e);
+  //   }
+
+  //   fromAdmin() {
+  //     socket?.emit("msg from admin", "hello roshan");
+  //   }
+  // }
 }
+
+// STEP1:  Stream setup
+// class StreamSocket {
+//   final _socketResponse = StreamController<String>();
+
+//   void Function(String) get addResponse => _socketResponse.sink.add;
+
+//   Stream<String> get getResponse => _socketResponse.stream;
+
+//   void dispose() {
+//     _socketResponse.close();
+//   }
+// }
